0bedc42ab9e459b5673a1840b19f4cf02b4865f6
Seonho Kim <seonhokim@Seonhoui-MacBookPro.local>
2023-03-27 21:10:12 +0900

    #2

---
+++ pomory-ios/Memory/Memo.swift
@@ -0,0 +1,21 @@
+//
+//  Memo.swift
+//  pomory-ios
+//
+//  Created by Seonho Kim on 2023/03/27.
+//
+
+import Foundation
+import SwiftUI
+
+class Memo: Identifiable, ObservedObject {
+    let id: UUID
+    @Published var content: String
+    let insertDate: Date
+    
+    init(content: String, insertDate: Date = Date.now) {
+        id = UUID()
+        self.content = content
+        self.insertDate = insertDate
+    }
+}
