4c32716396167ec0698ee32f6684d77fa051be38
Seonho Kim <seonhokim@Seonhoui-MacBookPro.local>
2023-03-27 19:59:57 +0900

    setting

---
+++ pomory-ios/MainListView.swift
@@ -0,0 +1,20 @@
+//
+//  MainListView.swift
+//  pomory-ios
+//
+//  Created by Seonho Kim on 2023/03/27.
+//
+
+import SwiftUI
+
+struct MainListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MainListView_Previews: PreviewProvider {
+    static var previews: some View {
+        MainListView()
+    }
+}
--- pomory-ios/pomory_iosApp.swift
+++ pomory-ios/pomory_iosApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct pomory_iosApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            MainListView()
         }
     }
 }
