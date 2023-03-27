e9848a9a0cae2f8fd14b914df7bba9f5b8791975
Seonho Kim <seonhokim@Seonhoui-MacBookPro.local>
2023-03-27 19:03:24 +0900

    project init

---
+++ pomory-ios/pomory_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  pomory_iosApp.swift
+//  pomory-ios
+//
+//  Created by Seonho Kim on 2023/03/27.
+//
+
+import SwiftUI
+
+@main
+struct pomory_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
